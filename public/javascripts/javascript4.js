46213a153ce4da6fc4ca1ee3a61f50034d2e3a6a
Bsampath21 <bsampath.bandaru@dispatchtrack.com>
2024-06-12 23:46:09 +0530

    Initial commit

---
+++ public/javascripts/javascript4.js
@@ -0,0 +1,14 @@
+// javascript1.js
+
+// Add a class to the active link in the navigation
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('header ul li a');
+    var currentPage = window.location.pathname.split('/').pop();
+
+    links.forEach(function(link) {
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        }
+    });
+});
+
